c452b90dd0f4acd553fd6f7d55e7337a3e1dffee
Kilian Martin <kilian.martin@me.com>
2017-08-22 14:49:09 +0200

    basics of booking controller working

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,14 +1,25 @@
 class BookingsController < ApplicationController
   def new
     @booking = Booking.new
+    @kitchen = Kitchen.find(params[:kitchen_id])
+    # @user = User.find(params[:user_id])
+    @user_id = 1
   end
 
   def create
-    @kitchen = Kitchen.find(params[:kitchen_id])
-    @booking = Bookings.new(booking_params)
+    @booking = Booking.create(booking_params)
+    @booking.kitchen = Kitchen.find(params[:kitchen_id])
+
+
+    # USER: NEXT LINE IS HOW WE WANT IT TO WORK IN THE END
+    # @booking.user = User.find(params[:user_id])
+
+    @booking.save
+
+    redirect_to kitchens_path
   end
 
   def booking_params
-    params.require(:booking).permit(:start_date, :end_date, :status)
+    params.require(:booking).permit(:start_date, :end_date, :status, :user_id)
   end
 end
