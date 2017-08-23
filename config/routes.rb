ef1de362758c53d190f93c3549f08aa76e02cc0f
John Hayes <john@ontheinternet.org.uk>
2017-08-23 14:02:47 +0200

    root changed, sign-in redirect work begun but commented out

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
-  root to: 'pages#home'
+  root to: 'kitchens#index'
 
   resources :kitchens, only: [ :index, :show ] do
     resources :bookings, only: [ :new, :create, :index ]
