c0318e1ec7a50800f0fac43cf72c87bd3fddaeff
Frederike Kugland <frederike.kugland@whu.edu>
2017-08-23 23:15:05 +0200

    photo function partially implemented, index view not functioning yet

--- app/controllers/kitchens_controller.rb
+++ app/controllers/kitchens_controller.rb
@@ -6,4 +6,26 @@ class KitchensController < ApplicationController
   def show
     @kitchen = Kitchen.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+    @kitchen = Kitchen.find(params[:id])
+  end
+
+  def update
+    @kitchen = Kitchen.find(params[:id])
+    @kitchen.update(kitchen_params)
+    redirect_to kitchen_path(@kitchen)
+  end
+
+  private
+
+  def kitchen_params
+    params.require(:kitchen).permit(:title, :address, :description, :photo)
+  end
 end
