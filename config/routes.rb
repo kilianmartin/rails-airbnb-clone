75302fb1a7d7e037899ad7e6a22826608053be8d
Frederike Kugland <frederike.kugland@whu.edu>
2017-08-23 11:53:52 +0200

    navbar function is added: overview, bookings overview

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-
+  resources :users, only: [ :show ]
   root to: "kitchens#index"
 
   resources :kitchens, only: [ :index, :show ] do
