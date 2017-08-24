8b291294daecf58481ba60fb7524739403c92818
John Hayes <john@ontheinternet.org.uk>
2017-08-24 18:15:23 +0200

    review form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170824075907) do
+ActiveRecord::Schema.define(version: 20170824153220) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,7 +55,7 @@ ActiveRecord::Schema.define(version: 20170824075907) do
   create_table "reviews", force: :cascade do |t|
     t.string "title"
     t.integer "rating"
-    t.string "comment"
+    t.text "comment"
     t.bigint "booking_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
