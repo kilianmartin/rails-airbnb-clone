639714f6fccf657196a5e5e5ac7864b7586cc2b3
Frederike Kugland <frederike.kugland@whu.edu>
2017-08-25 15:28:22 +0200

    seed set up, change dates per booking please

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,7 +29,14 @@ kitchen_contents = [
     price: 15
   },
   {
-    title: "English country-style kitchen",
+    title: "The Master Grill",
+    address: "Cowper St, London EC2A 4AP, Vereinigtes Königreich",
+    description: "Rooftop kitchen with everything that is needed for a perium grill experience.",
+    user: User.find(User.ids[1]),
+    price: 28
+  },
+  {
+    title: "Granny's kitchen",
     address: "Hauptstrasse 15, 10317 Berlin",
     description: "Rural charm in an urban setting",
     user: User.find(User.ids[1]),
@@ -48,6 +55,13 @@ kitchen_contents = [
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vitae venenatis velit. Vivamus porttitor nisi pulvinar, aliquet tellus vel, gravida.",
     user: User.find(User.ids[3]),
     price: 19
+  },
+  {
+    title: "Cupcake Backery",
+    address: "4/5 Elephant Rd, London SE17 1LB, Vereinigtes Königreich",
+    description: "Cute and girly kitchen, perfect for sweet backery sessions!",
+    user: User.find(User.ids[0]),
+    price: 15
   },
   {
     title: "Sushi kitchen",
@@ -71,6 +85,13 @@ kitchen_contents = [
     price: 25
   },
   {
+    title: "Stylish Soul Kitchen",
+    address: "17A Harrington Road, London, South Kensington SW7 3ES, Vereinigtes Königreich",
+    description: "Centrally located kitchen with inspiering architecture. Perfect for Asian soul cuisine cooking!",
+    user: User.find(User.ids[2]),
+    price: 40
+  },
+  {
     title: "Indonesian outdoor kitchen",
     address: "Falckensteinstr. 49, 10997 Berlin",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vitae venenatis velit. Vivamus porttitor nisi pulvinar, aliquet tellus vel, gravida.",
@@ -83,6 +104,13 @@ kitchen_contents = [
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vitae venenatis velit. Vivamus porttitor nisi pulvinar, aliquet tellus vel, gravida.",
     user: User.find(User.ids[8]),
     price: 50
+  },
+  {
+    title: "British country style",
+    address: "77A Charterhouse St, Clerkenwell, London EC1M 6HJ, Vereinigtes Königreich",
+    description: "This kitchen will remind you of your childhood, when visiting your granny in the country side. Sweet, elegant, and cozy.",
+    user: User.find(User.ids[3]),
+    price: 19
   },
 ]
 
@@ -97,44 +125,260 @@ for kitchen in Kitchen.ids
     end_date: Date.new(2017,8,25),
     status: "Pending",
     user_id: User.ids[1],
-    kitchen_id: kitchen
+    kitchen_id: 18
     },
      {
     start_date: Date.new(2017,8,21),
     end_date: Date.new(2017,8,25),
     status: "Pending",
     user_id: User.ids[2],
-    kitchen_id: kitchen
+    kitchen_id: 17
     },
      {
     start_date: Date.new(2017,8,21),
     end_date: Date.new(2017,8,25),
     status: "Pending",
     user_id: User.ids[3],
-    kitchen_id: kitchen
+    kitchen_id: 16
     },
      {
     start_date: Date.new(2017,8,21),
     end_date: Date.new(2017,8,25),
     status: "Pending",
     user_id: User.ids[4],
-    kitchen_id: kitchen
+    kitchen_id: 15
     },
      {
     start_date: Date.new(2017,8,21),
     end_date: Date.new(2017,8,25),
     status: "Pending",
     user_id: User.ids[5],
-    kitchen_id: kitchen
+    kitchen_id: 14
+    },
+
+
+    {
+    start_date: Date.new(2017,8,21),
+    end_date: Date.new(2017,8,25),
+    status: "Pending",
+    user_id: User.ids[6],
+    kitchen_id: 13
+    },
+     {
+    start_date: Date.new(2017,8,21),
+    end_date: Date.new(2017,8,25),
+    status: "Pending",
+    user_id: User.ids[7],
+    kitchen_id: 12
+    },
+     {
+    start_date: Date.new(2017,8,21),
+    end_date: Date.new(2017,8,25),
+    status: "Pending",
+    user_id: User.ids[8],
+    kitchen_id: 11
+    },
+     {
+    start_date: Date.new(2017,8,21),
+    end_date: Date.new(2017,8,25),
+    status: "Pending",
+    user_id: User.ids[9],
+    kitchen_id: 10
+    },
+     {
+    start_date: Date.new(2017,8,21),
+    end_date: Date.new(2017,8,25),
+    status: "Pending",
+    user_id: User.ids[10],
+    kitchen_id: 9
+    },
+
+
+     {
+    start_date: Date.new(2017,8,21),
+    end_date: Date.new(2017,8,25),
+    status: "Pending",
+    user_id: User.ids[11],
+    kitchen_id: 8
+    },
+     {
+    start_date: Date.new(2017,8,21),
+    end_date: Date.new(2017,8,25),
+    status: "Pending",
+    user_id: User.ids[12],
+    kitchen_id: 7
+    },
+     {
+    start_date: Date.new(2017,8,21),
+    end_date: Date.new(2017,8,25),
+    status: "Pending",
+    user_id: User.ids[13],
+    kitchen_id: 6
+    },
+
+
+    {
+    start_date: Date.new(2017,8,21),
+    end_date: Date.new(2017,8,25),
+    status: "Pending",
+    user_id: User.ids[14],
+    kitchen_id: 5
+    },
+     {
+    start_date: Date.new(2017,8,21),
+    end_date: Date.new(2017,8,25),
+    status: "Pending",
+    user_id: User.ids[15],
+    kitchen_id: 4
+    },
+     {
+    start_date: Date.new(2017,8,21),
+    end_date: Date.new(2017,8,25),
+    status: "Pending",
+    user_id: User.ids[16],
+    kitchen_id: 3
+    },
+     {
+    start_date: Date.new(2017,8,21),
+    end_date: Date.new(2017,8,25),
+    status: "Pending",
+    user_id: User.ids[17],
+    kitchen_id: 2
+    },
+     {
+    start_date: Date.new(2017,8,21),
+    end_date: Date.new(2017,8,25),
+    status: "Pending",
+    user_id: User.ids[18],
+    kitchen_id: 1
     }])
 end
 
 for booking in Booking.ids
-  Review.create({
-    title: Faker::Hacker.say_something_smart,
+  Review.create([{
+    title: Faker::Lorem.sentence,
     rating: rand(1..5),
     comment: Faker::Lorem.paragraph(3, false, 4),
     booking_id: 1
-  }
+  },
+
+  {
+    title: Faker::Lorem.sentence,
+    rating: rand(1..5),
+    comment: Faker::Lorem.paragraph(3, false, 4),
+    booking_id: 2
+  },
+
+  {
+    title: Faker::Lorem.sentence,
+    rating: rand(1..5),
+    comment: Faker::Lorem.paragraph(3, false, 4),
+    booking_id: 3
+  },
+
+  {
+    title: Faker::Lorem.sentence,
+    rating: rand(1..5),
+    comment: Faker::Lorem.paragraph(3, false, 4),
+    booking_id: 4
+  },
+
+  {
+    title: Faker::Lorem.sentence,
+    rating: rand(1..5),
+    comment: Faker::Lorem.paragraph(3, false, 4),
+    booking_id: 5
+  },
+
+  {
+    title: Faker::Lorem.sentence,
+    rating: rand(1..5),
+    comment: Faker::Lorem.paragraph(3, false, 4),
+    booking_id: 6
+  },
+
+  {
+    title: Faker::Lorem.sentence,
+    rating: rand(1..5),
+    comment: Faker::Lorem.paragraph(3, false, 4),
+    booking_id: 7
+  },
+
+  {
+    title: Faker::Lorem.sentence,
+    rating: rand(1..5),
+    comment: Faker::Lorem.paragraph(3, false, 4),
+    booking_id: 8
+  },
+
+  {
+    title: Faker::Lorem.sentence,
+    rating: rand(1..5),
+    comment: Faker::Lorem.paragraph(3, false, 4),
+    booking_id: 9
+  },
+
+  {
+    title: Faker::Lorem.sentence,
+    rating: rand(1..5),
+    comment: Faker::Lorem.paragraph(3, false, 4),
+    booking_id: 10
+  },
+
+  {
+    title: Faker::Lorem.sentence,
+    rating: rand(1..5),
+    comment: Faker::Lorem.paragraph(3, false, 4),
+    booking_id: 11
+  },
+
+  {
+    title: Faker::Lorem.sentence,
+    rating: rand(1..5),
+    comment: Faker::Lorem.paragraph(3, false, 4),
+    booking_id: 12
+  },
+
+  {
+    title: Faker::Lorem.sentence,
+    rating: rand(1..5),
+    comment: Faker::Lorem.paragraph(3, false, 4),
+    booking_id: 13
+  },
+
+  {
+    title: Faker::Lorem.sentence,
+    rating: rand(1..5),
+    comment: Faker::Lorem.paragraph(3, false, 4),
+    booking_id: 14
+  },
+
+  {
+    title: Faker::Lorem.sentence,
+    rating: rand(1..5),
+    comment: Faker::Lorem.paragraph(3, false, 4),
+    booking_id: 15
+  },
+
+  {
+    title: Faker::Lorem.sentence,
+    rating: rand(1..5),
+    comment: Faker::Lorem.paragraph(3, false, 4),
+    booking_id: 16
+  },
+
+  {
+    title: Faker::Lorem.sentence,
+    rating: rand(1..5),
+    comment: Faker::Lorem.paragraph(3, false, 4),
+    booking_id: 17
+  },
+
+  {
+    title: Faker::Lorem.sentence,
+    rating: rand(1..5),
+    comment: Faker::Lorem.paragraph(3, false, 4),
+    booking_id: 18
+  }]
   )
 end
